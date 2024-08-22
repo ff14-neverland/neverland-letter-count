29b3bf87412235f71ec4bbcb510b75df7e75a9ec
Sardo <sardo.ip@sardo.work>
2024-08-22 19:23:08 +0900

    Replace textract to new module

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const cors = require('@koa/cors');
-const textract = require('textract');
-const fs = require('fs');
-const lc = require('letter-count');
-const log4js = require('koa-log4')
+import Koa from 'koa';
+import koaBody from 'koa-body';
+import cors from '@koa/cors';
+import { readFile } from 'node:fs/promises'
+import { getTextExtractor } from 'office-text-extractor'
+import lc from 'letter-count';
+import log4js from 'koa-log4';
 
 const infoLogger = log4js.getLogger('info');
 infoLogger.level = 'info';
@@ -39,38 +39,35 @@ app.use(async ctx => {
     const fileKey = filesKeys[i];
     const filePath = files[fileKey].path;
     const fileType = files[fileKey].type;
-    const file = fs.readFileSync(filePath);
+    const file = await readFile(filePath);
     const count = await countText(name, file, fileType);
     totalCount+= count;
   }
   ctx.body = JSON.stringify({name, totalCount});
 });
 
-function countText(name, file, fileType){
-  return new Promise((resolve, reject) => {
-    textract.fromBufferWithMime(fileType, file, function(error, text) {
-      if(error){
-        errorLogger.error(error);
-      };
-      const regax = new RegExp('【' + name + '】([^【]+)', 'g');
-      const contents = text.match(regax);
-      let charsCount = 0; 
-      if(contents){
-        for(let i = 0; i < contents.length; i++){
-          const content = contents[i];
-          const extraRegax = new RegExp('(【' + name + '】)([^【]+)', 'g');
-          const baseText = extraRegax.exec(content)[2];
-          const cleanText = baseText.replace(/ /g, '');
-          const chars = (lc.count(cleanText.trim(), '-c')).chars;
-          charsCount += chars;
-        }
-      }else{
-        const cleanText = text.replace(/ /g, '');
+async function countText(name, file, fileType){
+  return new Promise(async (resolve, reject) => {
+    const extractor = getTextExtractor();
+    const text = await extractor.extractText({ input: file, type: 'buffer' });
+    const regax = new RegExp('【' + name + '】([^【]+)', 'g');
+    const contents = text.match(regax);
+    let charsCount = 0; 
+    if(contents){
+      for(let i = 0; i < contents.length; i++){
+        const content = contents[i];
+        const extraRegax = new RegExp('(【' + name + '】)([^【]+)', 'g');
+        const baseText = extraRegax.exec(content)[2];
+        const cleanText = baseText.replace(/ /g, '');
         const chars = (lc.count(cleanText.trim(), '-c')).chars;
         charsCount += chars;
       }
-      resolve(charsCount);
-    });
+    }else{
+      const cleanText = text.replace(/ /g, '');
+      const chars = (lc.count(cleanText.trim(), '-c')).chars;
+      charsCount += chars;
+    }
+    resolve(charsCount);
   });
 };
 
